af849e57d776e3de755e05f04daead5d4dfb823b
Arthur Wilton <arthur.m.wilton@gmail.com>
2020-09-11 06:37:20 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,9 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+    name_array = []
+    data.each { |main_category, second_category| second_category.each { |third_category, name| name_array.push(name) } }
+    new_hash = name_array.flatten.uniq.to_h { |x| [x, {:color => [], :gender => [], :lives => []}] }
+    data[:color].each { |color, name_list| name_list.each {|name| new_hash[name][:color].push(color.to_s) } }
+    data[:gender].each { |gender, name_list| name_list.each {|name| new_hash[name][:gender].push(gender.to_s) } }
+    data[:lives].each { |lives, name_list| name_list.each {|name| new_hash[name][:lives].push(lives.to_s) } }
+    new_hash
 end
