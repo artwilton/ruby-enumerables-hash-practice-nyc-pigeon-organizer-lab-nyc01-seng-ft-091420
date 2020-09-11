8bf3b47b5f7edcb520057dd120336a948e838a37
Arthur Wilton <arthur.m.wilton@gmail.com>
2020-09-11 06:37:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,5 +5,4 @@ def nyc_pigeon_organizer(data)
     data[:color].each { |color, name_list| name_list.each {|name| new_hash[name][:color].push(color.to_s) } }
     data[:gender].each { |gender, name_list| name_list.each {|name| new_hash[name][:gender].push(gender.to_s) } }
     data[:lives].each { |lives, name_list| name_list.each {|name| new_hash[name][:lives].push(lives.to_s) } }
-    new_hash
 end
